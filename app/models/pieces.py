2217e0d3a92129ca5b3fc02d58c74a13f9b2bd3c
Stefanos Dimitriou <93728701+TheraOzz@users.noreply.github.com>
2024-10-06 18:22:47 +0300

    add validate_move method for queen

--- app/models/pieces.py
+++ app/models/pieces.py
@@ -1,6 +1,7 @@
 import numpy as np
 
 from app.models.common import PieceMove
+from app.errors import WrongMoveError
 
 
 class Piece:
@@ -33,7 +34,24 @@ class Rook(Piece):
 
 class Queen(Piece):
     def validate_move(self, board: np.array, move: PieceMove):
-        pass
+        super().validate_move(board, move)
+
+        # Check if the move is in the same column
+        if move.from_pos[1] == move.to_pos[1]:
+            print("valid column move")
+            return
+
+        # Check if the move is in the same row
+        if move.from_pos[0] == move.to_pos[0]:
+            print("valid row move")
+            return
+
+        # Check if the move is diagonal
+        if abs(move.from_pos[0] - move.to_pos[0]) == abs(move.from_pos[1] - move.to_pos[1]):
+            print(f"valid diagonal move")
+            return True
+
+        raise WrongMoveError()
 
 
 class King(Piece):
