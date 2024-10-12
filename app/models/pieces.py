b674d8b6be2cd30cdb656d9963350854a9f5f7d1
Stefanos Dimitriou <93728701+TheraOzz@users.noreply.github.com>
2024-10-12 20:21:22 +0300

    adapt to from_pos and to_pos updates

--- app/models/pieces.py
+++ app/models/pieces.py
@@ -26,7 +26,16 @@ class Knight(Piece): ...
 class Bishop(Piece): ...
 
 
-class Rook(Piece): ...
+class Rook(Piece):
+    def validate_move(self, board: np.array, move: PieceMove):
+        super().validate_move(board, move)
+        if move.from_pos[0] == move.to_pos[0]:
+            print("same column")
+            return
+        if move.from_pos[1] == move.to_pos[1]:
+            print("same row")
+            return
+        raise WrongMoveError()
 
 
 class Queen(Piece): ...
